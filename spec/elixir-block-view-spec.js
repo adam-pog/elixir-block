99832db0ca94c70cec024ecbf6326c528cc8ad10
Adam Pogwizd <adampog77@gmail.com>
2018-05-18 13:17:12 -0500

    Initial package generation

---
+++ spec/elixir-block-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ElixirBlockView from '../lib/elixir-block-view';
+
+describe('ElixirBlockView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
